0a4831d87c52b97d4f2dc7be05cf0f5ff48cfcef
ThomasAkram <91660351+ThomasAkram@users.noreply.github.com>
2023-01-27 11:25:19 +0700

    ....

--- sipatas/Pengajuan_Pembelian/01DaftarHargaSapi.php
+++ sipatas/Pengajuan_Pembelian/01DaftarHargaSapi.php
@@ -26,7 +26,7 @@
                     <div class="col-lg-6">
                         <div class="text-center my-5">
                             <h1 class="display-5 fw-bolder text-white mb-2">Pembelian Sapi</h1>
-                            <p class="lead text-white-50 mb-4">Disini berisi informasi singkat tentang xxxxxx</p>
+                            <p class="lead text-white-50 mb-4">Disini berisi informasi mengenai Jenis Sapi dan Harga Sapi</p>
                             <div class="d-grid gap-3 d-sm-flex justify-content-sm-center">
                               <img src="../halaman/img/DaftarHargaSapi.png" height="1000"                     
                             </div>
