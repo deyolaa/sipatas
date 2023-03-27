8aa14dc30b655b7eada1607ccbae3ac93a965fbc
ThomasAkram <91660351+ThomasAkram@users.noreply.github.com>
2023-03-27 23:49:42 +0700

    tambah wassaapp

--- sipatas/Pengajuan_Pembelian/01DetailPPS.php
+++ sipatas/Pengajuan_Pembelian/01DetailPPS.php
@@ -80,6 +80,7 @@
                         while ($row= mysqli_fetch_assoc($result)){
                     ?>
                     <h5 class="text-black my-3">Detail Pengajuan <?php echo $row['instansi_pengajuan']?></h5>
+                    <a type="button" class="btn btn-primary mb-3 mx-3" style="background-color: green;" onclick="location.href='https://api.whatsapp.com/send?phone=<?php echo $row['nomor_pengajuan'];?>';"><i class="bi bi-whatsapp"></i> Whatsapp</a>
                     <table class="table text-black table-sm table-bordered mx-3"  id="dataTable" width="100%" cellspacing="0">
                     
                                 <tr>   
